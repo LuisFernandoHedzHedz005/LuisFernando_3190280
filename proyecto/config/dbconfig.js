76166047f1f6352c1c72857ef42b0946f69b188e
LuisFernandoHedzHedz005 <luisfdohernandez005@gmail.com>
2024-11-26 04:55:44 -0600

    Prueba contraseña

--- proyecto/config/dbconfig.js
+++ proyecto/config/dbconfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', // tu usuario de MySQL
-    password: '123456', // tu contraseña de MySQL
+    password: '1234', // tu contraseña de MySQL
     database: 'celulares'
 });
 
